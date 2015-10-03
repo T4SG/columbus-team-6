709a72f5258f45b96d2f955c3c6d9ec66fb143c8
Stephen Meriwether <smeriwether714@gmail.com>
2015-10-02 23:56:12 -0400

    Added admin page

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,20 @@
 class LoginController < ApplicationController
   def index
+    user = params["user"]
+    email = user.nil? ? nil : user["email"]
+    if email.blank?
+      @user = User.new
+    else
+      if admin?(email)
+        redirect_to admin_index_url
+      else
+        redirect_to calendar_index_url
+      end
+    end
+  end
+
+  def admin?(email)
+    email == "admin@ljff.com"
   end
 end
 
