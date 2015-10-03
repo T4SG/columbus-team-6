05209838d89a1b853b8b34fc4ea8880abd09f55f
Stephen Meriwether <smeriwether714@gmail.com>
2015-10-03 00:23:15 -0400

    Update README

---
+++ git.sh
@@ -0,0 +1,15 @@
+rdoc="$(find . -name 'README.rdoc')"
+md="$(find . -name 'README.md')"
+
+if [ "$rdoc" == "" ]
+  then
+    mv ./README.md ./README.rdoc
+  else
+    mv ./README.rdoc ./README.md
+fi
+
+git add .
+git ci -m "Update README"
+git pull origin master
+git push origin master
+
